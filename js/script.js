d3c3c4bfe886fe97bced84a74553f00cc626bef3
Joaquín Fernandez <xcopako@gmail.com>
2020-02-11 21:51:58 -0300

    Added a party mode
    whaaat? party mode?
    Just to try out some js features

--- js/script.js
+++ js/script.js
@@ -21,7 +21,57 @@ function checkTime(i) {
     return i;
 }
 
-setInterval(counter,500);
+var zaWarudo =  setInterval(counter,500);
+
+
+  //Party time events and functions
+var partyButton = document.querySelector('button');
+var body = document.querySelector('body');
+var p = document.querySelector('p');
+var hTitle = document.querySelector('h1');
+var partyActivator;
+
+function partyEvent(){
+  //al clickear, se chekea el estado, como party
+  //activator no tiene valor, pasa al else donde
+  // se le otorga el valor 2, en donde al mismo tiempo
+  // se activa el PARTY MODE, pero esto causa que 
+  //al apretar nuevamente, party activator tenga el valor 2
+  //y se activa en el "IF"
+  // el cual apaga el party mode.
+  if (partyActivator === 2){
+    partyActivator = 1;
+
+    partyButton.innerHTML='Party Time';
+    partyButton.style.backgroundColor = 'white';
+    partyButton.style.color = 'black';
+    body.style.background = "url('../background-img.jpg')";
+    body.style.color = "white";
+    hTitle.innerHTML = "Super serious clock";
+
+  }else{
+    partyActivator = 2;
+
+    partyButton.innerHTML="End party..";
+    partyButton.style.backgroundColor = "red";
+    partyButton.style.color = "white"; 
+    body.style.background = "url('../party.gif')";
+    body.style.backgroundSize = "cover";
+    body.style.color = "pink";
+    body.style.textShadow = "4px 4px 1px black";
+    partyButton.style.boxShadow = "4px 4px 5px black";
+    hTitle.innerHTML = "Party CloOoOoOock";
+
+
+  }
+
+}
+
+
+partyButton.addEventListener('click', partyEvent);
+
+
+
 
 
 
